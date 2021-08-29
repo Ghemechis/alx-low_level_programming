be30907c60bace1042e9b7d672036ce749105d9a
Ghemechis <kiya.order@gmail.com>
2021-08-29 08:13:04 -0700

    script that prints text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #!/bin/bash
-#include <stdio.c>
-int main()
+#include <stdio.h>
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
-	getchar();
-	return 0;
+	return (0);
 }
