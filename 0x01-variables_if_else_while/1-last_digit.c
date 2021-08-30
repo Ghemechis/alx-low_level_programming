00f51a60214d46c3b2bb0513eb6f1a60ce3ac435
Ghemechis <kiya.order@gmail.com>
2021-08-30 10:28:48 -0700

    script that display the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,6 @@ int main(void)
 {
 	int n;
 	int l;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
@@ -21,11 +20,11 @@ int main(void)
 	}
 	else if (l == 0)
 	{
-	  printf("Last digit of %d is %d and 0\n", n, l);
+	  printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	else if ((l < 6 ) && (l != 0)) 
-	{
-	  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-	}
+	else if ((l < 6) && (l != 0))
+	  {
+	    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	  }
 	return (0);
 }
