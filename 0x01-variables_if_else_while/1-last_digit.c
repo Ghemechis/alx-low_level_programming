f3f818b6b3f953eac2827321bebfe23a2ea297c4
Ghemechis <kiya.order@gmail.com>
2021-08-30 08:21:10 -0700

    script that classify the last digit digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,8 @@
 int main(void)
 {
 	int n;
-
+	int l;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
@@ -27,6 +28,7 @@ int main(void)
 	  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	else {
-	  return (0);
+	  printf("Error\n");
 	}
+	return (0);
 }
