7c4a892e6250a4b61baa038ae5c7d1536c304819
Ghemechis <kiya.order@gmail.com>
2021-08-30 10:15:11 -0700

    script that display the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else if (l == 0)
 	{
-	  printf("Last digit of %d is %d and zero\n", n, l);
+	  printf("Last digit of %d is %d and 0\n", n, l);
 	}
 	else if ((l < 6 ) && (l != 0)) 
 	{
