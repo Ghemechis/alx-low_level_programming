7e9f13d213112caf0f3da1bf1d3ea9d5916f6070
Ghemechis <kiya.order@gmail.com>
2021-08-30 08:15:23 -0700

    Script that display last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,22 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
+	l = n % 10;
+	if (l > 5)
 	{
-		printf("%d is positive\n", n);
-	else if(n=0)
+	  printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
 	{
-		printf("%d is zero\n", n);
-	else 
+	  printf("Last digit of %d is %d and zero\n", n, l);
+	}
+	else if ((l < 6 ) && (l != 0)) 
 	{
-		printf("%d is negative\n", n);
+	  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	else {
+	  return (0);
 	}
 }
